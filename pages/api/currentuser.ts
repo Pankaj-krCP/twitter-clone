88cb1ad0d70f48e3a016dd7678abdc0ce0ce018a
Pankaj-krCP <pankajkumar70792@gmail.com>
2024-03-30 14:36:42 +0530

    hey

--- pages/api/currentuser.ts
+++ pages/api/currentuser.ts
@@ -5,15 +5,12 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method !== "GET") {
-    return res.status(405).end();
-  }
-  return res.status(200).json("Testing");
-  try {
-    const { currentUser } = await serverAuth(req);
-    return res.status(200).json(currentUser);
-  } catch (error) {
-    console.log(error);
-    return res.status(400).end();
-  }
+  res.status(200).json({ text: "Testing" });
+  // try {
+  //   const { currentUser } = await serverAuth(req);
+  //   return res.status(200).json(currentUser);
+  // } catch (error) {
+  //   console.log(error);
+  //   return res.status(400).end();
+  // }
 }
